88fe71f8377427373a037fe4b8569e1535b8d98b
Edwin Vega <edwega@gmail.com>
2018-02-24 18:31:23 -0600

    Exercise on Tables

--- 12_InterviewQuestions/src/test/Tables.java
+++ 12_InterviewQuestions/src/test/Tables.java
@@ -15,6 +15,7 @@ public class Tables {
 		System.setProperty("webdriver.gecko.driver", 
 				"/home/edwin/Udemy/SeleniumWebDriver/drivers/geckodriver");
 		WebDriver driver = new FirefoxDriver();
+		int scoreSum = 0;
 		
 		driver.get("http://www.cricbuzz.com/live-cricket-scorecard/20035/ken-vs-nep-10th-match-icc-world-cricket-league-division-two-2018");
 		
@@ -25,7 +26,22 @@ public class Tables {
 					By.cssSelector("div[class='cb-col cb-col-100 cb-scrd-itms'] div:nth-child(3)")));
 		
 		for (int i = 0; i < (scores.size() - 2); i++) {
-			System.out.println(scores.get(i).getText());
+			scoreSum += Integer.parseInt(scores.get(i).getText());
 		}
+		
+		System.out.println("Score sum: " + scoreSum);
+		String extras = driver.findElement(
+				By.xpath("//div[text()='Extras']/following-sibling::div")).getText();
+		
+		System.out.println("Extras: " + extras);
+		scoreSum += Integer.parseInt(extras);
+		System.out.println("Total sum: " + scoreSum);
+		
+		String total = driver.findElement(
+				By.xpath("//div[text()='Total']/following-sibling::div")).getText(); 
+		System.out.println("Total: " + total);
+		
+		System.out.println(scoreSum == Integer.valueOf(total) ?
+				"Count matches" : "Count fails!");
 	}
 }
