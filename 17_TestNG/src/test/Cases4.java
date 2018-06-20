5f31800762685d48bdb86cd94035d4c7197d079a
Vega <Edwin.Vega@dieboldnixdorf.com>
2018-06-20 16:09:56 -0500

    TestNG Annotations, data providers

--- 17_TestNG/src/test/Cases4.java
+++ 17_TestNG/src/test/Cases4.java
@@ -1,14 +1,16 @@
 package test;
 
+import org.testng.annotations.DataProvider;
 import org.testng.annotations.Parameters;
 import org.testng.annotations.Test;
 
 public class Cases4 {
 
-	@Test
-	public void test1() {
+	@Test(dataProvider="getData")
+	public void test1(String username, String pass) {
 		System.out.println("Cases4.Test1");
-	}
+		System.out.println("USER: " + username + " PASS: " + pass);
+	} 
 	
 	@Parameters({"user", "pass"})
 	@Test
@@ -21,5 +23,21 @@ public class Cases4 {
 	@Test(groups={"group1"})
 	public void test3(String url) {
 		System.out.println("Cases4.Test3 " + "URL: " + url);
+	}
+	
+	@DataProvider
+	public Object[][] getData() {
+		Object[][] data = new Object[3][2];
+		
+		data[0][0] = "firstUserName";
+		data[0][1] = "password";
+
+		data[1][0] = "2ndSetUsername";
+		data[1][1] = "2ndPass";
+		
+		data[2][0] = "3rdSetUsername";
+		data[2][1] = "3rdPass";
+		
+		return data;
 	}
 }
